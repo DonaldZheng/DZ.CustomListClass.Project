6f8c6931d40ce1e00287919c572c0768ed5ee352
DonaldZheng <dzheng112@gmail.com>
2021-03-01 13:43:31 -0600

    added two instances in the Overload Unit test, creating one more before going to the Overload Method

--- CustomListLutetiumUnitTesting/OverloadToAddTwoInstances.cs
+++ CustomListLutetiumUnitTesting/OverloadToAddTwoInstances.cs
@@ -6,12 +6,42 @@ using CustomListLutetium;
 namespace CustomListLutetiumUnitTesting
 {
     [TestClass]
-    class OverloadToAddTwoInstances // take values in two different list and make it one 
+    // JJ and David said it's okay to have 3 UnitTesting here as well
+    public class OverloadToAddTwoInstances // take values in two different list and make it one 
     {
         [TestMethod]
-        public void test()
+        public void AddTwoInstance_WithTwoList()
         {
-        
+            CustomList<int> listOne = new CustomList<int>() { 1, 3, 5 };
+            CustomList<int> listTwo = new CustomList<int>() { 2, 4, 6 };
+            CustomList<int> totalOfTwoList;
+
+            //Arrange 
+            int expected = 6;
+            int actual;
+
+            //Act
+            totalOfTwoList = listOne + listTwo;
+            actual = totalOfTwoList.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+
+        public void AddTwoInstance_WithInstructors()
+        {
+            CustomList<string> instructorGroupOne = new CustomList<string>() { "JJ", "Brent", "Brett", "Charles" };
+            CustomList<string> instructorGroupTwo = new CustomList<string>() { "Nevin", "Michael", "Pascal", "David" };
+            //Arrange 
+            CustomList<string> expected = new CustomList<string>() { "JJ", "Brent", "Brett", "Charles", "Nevin", "Michael", "Pascal", "David" };
+            CustomList<string> actual;
+
+            //Act
+            actual = instructorGroupOne + instructorGroupTwo;
+
+            //Assert 
+            Assert.AreEqual(expected, actual);
         }
     }
 }
